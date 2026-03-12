87cb29491b8890ab09df2293854329176458f61a
VBubols <vitorbubols@gmail.com>
2026-03-12 19:38:14 -0300

    [JS, NODE] resolved ex05

--- Desenvolvimento_de_sistemas/exercicios_Node/Ex05/normalizadorCaminhos.js
+++ Desenvolvimento_de_sistemas/exercicios_Node/Ex05/normalizadorCaminhos.js
@@ -1,18 +1,15 @@
 import { resolve, join, basename } from 'path'
 
 export async function normalizarCaminhos(){
-
-    const arrayCaminhos = [
-        './textos/text.txt',
-        '../images/photo.jpg',
-        ',/audios/msc.mp3'
-    ]
-
     try {
-        for(const caminho of arrayCaminhos){
-            const nomeArquivo = await basename(caminho)
-            console.log(nomeArquivo);
-            
+        const caminho1 = join("caminho1", "text.json")
+        const caminho2 = join("caminho2", "subcaminho", "text.txt")
+        const caminhos = [caminho1, caminho2]
+        for(const [i, caminho] of caminhos.entries()){
+            console.log(`---Caminho ${i + 1}---`);
+            console.log(`Caminho do arquivo: ${caminho}`);
+            console.log(`Nome do arquivo: ${basename(caminho)}`)
+            console.log(`Caminho absoluto: ${resolve(caminho)}\n`)
         }
     } catch (error) {
         console.log(`Erro: ${error}`);
