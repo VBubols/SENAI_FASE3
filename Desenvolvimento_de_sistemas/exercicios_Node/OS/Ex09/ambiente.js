597b21ea38acbac5903a48f3e580b155805773b8
VBubols <vitorbubols@gmail.com>
2026-03-29 23:22:32 -0300

    [NODE, JS] added exercises 9 and 10

---
+++ Desenvolvimento_de_sistemas/exercicios_Node/OS/Ex09/ambiente.js
@@ -0,0 +1,39 @@
+import { readFile, writeFile, access } from 'fs/promises'
+import { homedir, uptime, hostname } from 'node:os'
+
+async function verificarArquivo(){
+    try {
+        await access('./relatorio.txt')
+        console.log("Relatório já existe!");
+        return true
+    } catch {
+        console.log("Arquivo ainda não existe!");
+        return false
+    }
+}
+
+async function lerAmbiente(){
+    try {
+        const home = homedir()
+        const uptimePC = uptime() 
+        const hostnamePC = hostname()
+        //console.log(`Home: ${home}\nUptime: ${uptimePC}\nHost: ${hostnamePC}`)
+        const conteudo = {home, uptimePC, hostnamePC}
+        return conteudo
+    } catch (error) {
+        console.log(`ERRO: ${error}`)
+    }
+}
+
+export async function relatorioAmbiente(){
+    try {
+        if(await verificarArquivo()){
+            return
+        }
+        const conteudoRelatorio = await lerAmbiente()
+        await writeFile('./relatorio.txt', JSON.stringify(conteudoRelatorio))
+        console.log("Arquivo criado com sucesso!")
+    } catch (error) {
+        console.log(`ERRO: ${error}`)
+    }
+}
---
+++ Desenvolvimento_de_sistemas/exercicios_Node/OS/Ex10/compararAmbientes.js
@@ -0,0 +1,18 @@
+import { platform } from 'node:os'
+
+export async function identificarAmbiente(){
+    try {
+        const ambiente = platform()
+        if (ambiente == "win32"){
+            console.log("Você está em um Windows! Espero que não seja o 11 :)")
+        } if (ambiente == "darwin") {
+            console.log("Você está em um MAC. $$$$")
+        } if (ambiente == "linux") {
+            console.log("Você está em um Linux. Viva Linus Torvalds!!")
+        }
+    } catch (error) {
+        console.log(`ERRO: ${error}`)
+    }
+}
+
+identificarAmbiente()
