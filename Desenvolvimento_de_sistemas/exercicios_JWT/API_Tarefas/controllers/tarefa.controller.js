ccee5b0c9a6607928ec47fd69dd2f0c527fac096
VBubols <vitorbubols@gmail.com>
2026-04-27 21:43:58 -0300

    created dir for new JWT exercise

---
+++ Desenvolvimento_de_sistemas/exercicios_JWT/API_Tarefas/controllers/tarefa.controller.js
@@ -0,0 +1,60 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import * as model from '../models/clientesModel.js';
+
+export async function cadastrarCliente(req, res){
+    const { nome, email, senha } = req.body;
+
+    try {
+        const existingUser = await model.buscarPorEmail(email);
+        if(existingUser){
+            return res.status(400).json({mensagem: 'Usuário já existe'})
+        }
+
+        const user = await model.cadastrarCliente(nome, email, senha);
+        delete user.senha; //necessário deletar a senha não criptografada
+
+        return res.status(201).json({
+            mensagem: 'Usuário criado com sucesso!',
+            user
+        });
+    } catch (error) {
+        console.log(`Erro no controller cadastrarCliente: ${error}`)
+    }
+};
+
+export async function login(req, res) {
+    const { email, senha } = req.body;
+
+    try {
+        const user = await model.buscarPorEmail(email);
+        if(!user){
+            return res.status(404).json({mensagem: 'Usuário não encontrado'})
+        };
+
+        const compararSenha = bcrypt.compare(senha, user.senha);
+        if(!compararSenha){
+            return res.status(400).json({mensagem: 'Credenciais inválidas'})
+        };
+        delete user.senha;
+
+        const userToken = jwt.sign(
+            user,
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        );
+
+        res.json({token: userToken, user: user});
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export async function perfil(req, res) {
+    try {
+        return res.status(200).json(req.user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
